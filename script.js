911a022d5272e7e25e3d26bafc34cf5f3b8c58c6
ddvdv <vdv.david@gmail.com>
2017-06-05 18:00:52 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,41 @@
+var template = function(text) {
+  return '<p><input type="checkbox"><i class="glyphicon glyphicon-star"></i><span>' + text + '</span><i class="glyphicon glyphicon-remove"></i></p>';
+};
+
+var add = function(item) {
+  var html = template(item);
+  $(html).appendTo('div .list');
+  return false;
+};
+
+
+var main = function() {
+  $('form').submit(function() {
+      var text = $('#todo').val();
+      add(text);
+    	//$(html).appendTo('div .list');
+    	$('#todo').val(''); 
+    return false; 
+  });
+ 	$('.list').on('click', '.glyphicon-star', function(){
+    $(this).toggleClass('active');
+  });
+  $('.list').on('click', '.glyphicon-remove', function(){
+    $(this).parent().slideUp('fast', 'swing');
+  });
+  
+  // speech recognition
+  if (annyang) {
+
+    var commands = {
+      'add *tag': add
+    };
+
+    annyang.addCommands(commands);
+
+    annyang.start();
+  };
+  
+};
+
+$(document).ready(main);
